3cc739d1fafa4878a7c019a70c6676c871b370bf
Madis <kalashnikov.madis@gmail.com>
2022-08-29 10:44:38 +0300

    Add input, delete and clear functions Add comments On number btn click display textcontent changes On delete btn click display textcontent last string gets deleted On clear btn click display textcontent = ""

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,62 @@
+// addition func
 const add = (a, b) => {
-	return a + b
+	return a + b;
 }
 
+// substraction func
 const substract = (a, b) => {
-	return a - b
+	return a - b;
 }
 
+// multiplication func
 const multiply = (a, b) => {
-	if (a <= 1) return b
-	if (b <= 1) return a
-	return a * b
+	if (a <= 1) return b;
+	if (b <= 1) return a;
+	return a * b;
 }
 
+// divide func
 const divide = (a, b) => {
-	if (a === 0) return "ERR"
-	if (b === 0) return "ERR"
-	return (a / b).toFixed(3)
+	if (a === 0) return "ERR";
+	if (b === 0) return "ERR";
+	return (a / b).toFixed(3);
 }
 
+// operate func
 const operate = (operator, a, b) => {
-	return operator(a, b)
+	return operator(a, b);
 }
 
-const operators = document.querySelectorAll("[data-operator]")
-const numbers = document.querySelectorAll("[data-number]")
-console.log(operators, numbers)
+// initialize operators and numbers
+const operators = document.querySelectorAll("[data-operator]");
+const numbers = document.querySelectorAll("[data-number]");
+
+// initialize display for calc
+const display = document.querySelector("#calc-display");
+// set display textcontent based on input values
+const displayNumbers = (e) => {
+	const num = e.target.dataset.number;
+	display.textContent += `${num}`
+}
+// for each number btn add onclick displaynumbers func
+numbers.forEach(num => num.addEventListener("click", displayNumbers));
+
+// delete last character from display textcontent
+const deleteLastDigit = () => { 
+	display.textContent = display.textContent.slice(0, -1);
+}
+
+// initialize delete buttons
+const deleteBtn = document.querySelector("#delete")
+// add onclick delete last digit func
+deleteBtn.addEventListener("click", deleteLastDigit)
+
+// initialize clear button
+const clearBtn = document.querySelector("#clear")
+// clear display text content func
+const clearDisplay = () => {
+	display.textContent = ""
+}
+// add onclick clear display func
+clearBtn.addEventListener("click", clearDisplay)
 
